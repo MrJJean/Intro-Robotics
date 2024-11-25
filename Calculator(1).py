386068bdd099ce3df639b3214cdaf5e773bb981d
MrJJean <jjean36@schools.nyc.gov>
2024-11-25 10:02:14 -0500

    Calculator
    Math Operations Calculator

---
+++ Calculator(1).py
@@ -0,0 +1,14 @@
+a = 9
+b = 8
+
+addition = a+b
+subtraction = a-b
+multiplication = a*b
+division = a/b
+power = a**b 
+
+print (addition)
+print (subtraction)
+print (multiplication)
+print (division)
+print (power)
